4562e1444361b035bda81af6bc77ea4b5e1920b3
Mohamed Faizal <mofaizal@microsoft.com>
2024-04-20 09:40:33 +0800

    output updated

--- examples/kv_selfssl_waf_https_app_gateway/pre-requisites-outputs.tf
+++ examples/kv_selfssl_waf_https_app_gateway/pre-requisites-outputs.tf
@@ -1,186 +1,186 @@
-# Output for Resource Group
-output "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  value       = azurerm_resource_group.rg-group.name
-}
-
-# Output for Virtual Network
-output "virtual_network_name" {
-  description = "Name of the Azure Virtual Network"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-# Output for Subnets
-output "frontend_subnet_name" {
-  description = "Name of the Frontend Subnet"
-  value       = azurerm_subnet.frontend.name
-}
-
-output "backend_subnet_name" {
-  description = "Name of the Backend Subnet"
-  value       = azurerm_subnet.backend.name
-}
-
-output "workload_subnet_name" {
-  description = "Name of the Workload Subnet"
-  value       = azurerm_subnet.workload.name
-}
-
-output "private_ip_test_subnet_name" {
-  description = "Name of the Private IP Test Subnet"
-  value       = azurerm_subnet.private-ip-test.name
-}
-
-# output "bastion_subnet_name" {
-#   description = "Name of the Bastion Host Subnet"
-#   value       = azurerm_subnet.bastion.name
+# # Output for Resource Group
+# output "resource_group_name" {
+#   description = "Name of the Azure Resource Group"
+#   value       = azurerm_resource_group.rg-group.name
 # }
 
-# Output for Log Analytics Workspace
-output "log_analytics_workspace_name" {
-  description = "Name of the Azure Log Analytics Workspace"
-  value       = azurerm_log_analytics_workspace.log_analytics_workspace.name
-}
-
-# # Output for Windows Virtual Machine (Bastion)
-# output "bastion_virtual_machine_name" {
-#   description = "Name of the Windows Virtual Machine (Bastion Host)"
-#   value       = azurerm_windows_virtual_machine.bastion.name
+# # Output for Virtual Network
+# output "virtual_network_name" {
+#   description = "Name of the Azure Virtual Network"
+#   value       = azurerm_virtual_network.vnet.name
 # }
 
-# # Output for Network Interface (Bastion NIC)
-# output "bastion_nic_name" {
-#   description = "Name of the Network Interface (NIC) for Bastion Host"
-#   value       = azurerm_network_interface.bastion_win_vm_nic.name
+# # Output for Subnets
+# output "frontend_subnet_name" {
+#   description = "Name of the Frontend Subnet"
+#   value       = azurerm_subnet.frontend.name
 # }
 
-# # Output for Public IP (Bastion Public IP)
-# output "bastion_public_ip_name" {
-#   description = "Name of the Public IP Address for Bastion Host"
-#   value       = azurerm_public_ip.bastion_public_ip.name
+# output "backend_subnet_name" {
+#   description = "Name of the Backend Subnet"
+#   value       = azurerm_subnet.backend.name
 # }
 
-# # Output for Bastion Host
-# output "bastion_host_name" {
-#   description = "Name of the Azure Bastion Host"
-#   value       = azurerm_bastion_host.bastion-host.name
+# output "workload_subnet_name" {
+#   description = "Name of the Workload Subnet"
+#   value       = azurerm_subnet.workload.name
 # }
 
-# # Output for Linux Virtual Machine Scale Set
-# output "linux_vmss_name" {
-#   description = "Name of the Linux Virtual Machine Scale Set"
-#   value       = azurerm_linux_virtual_machine_scale_set.app_gateway_web_vmss.name
+# output "private_ip_test_subnet_name" {
+#   description = "Name of the Private IP Test Subnet"
+#   value       = azurerm_subnet.private-ip-test.name
 # }
 
-# # Output for Network Security Group
-# output "network_security_group_name" {
-#   description = "Name of the Network Security Group (NSG)"
-#   value       = azurerm_network_security_group.ag_subnet_nsg.name
+# # output "bastion_subnet_name" {
+# #   description = "Name of the Bastion Host Subnet"
+# #   value       = azurerm_subnet.bastion.name
+# # }
+
+# # Output for Log Analytics Workspace
+# output "log_analytics_workspace_name" {
+#   description = "Name of the Azure Log Analytics Workspace"
+#   value       = azurerm_log_analytics_workspace.log_analytics_workspace.name
 # }
 
-# # Output for NSG Rule Inbound Ports
-# output "inbound_ports_map" {
-#   description = "Map of NSG inbound rule ports"
-#   value       = local.ag_inbound_ports_map
+# # # Output for Windows Virtual Machine (Bastion)
+# # output "bastion_virtual_machine_name" {
+# #   description = "Name of the Windows Virtual Machine (Bastion Host)"
+# #   value       = azurerm_windows_virtual_machine.bastion.name
+# # }
+
+# # # Output for Network Interface (Bastion NIC)
+# # output "bastion_nic_name" {
+# #   description = "Name of the Network Interface (NIC) for Bastion Host"
+# #   value       = azurerm_network_interface.bastion_win_vm_nic.name
+# # }
+
+# # # Output for Public IP (Bastion Public IP)
+# # output "bastion_public_ip_name" {
+# #   description = "Name of the Public IP Address for Bastion Host"
+# #   value       = azurerm_public_ip.bastion_public_ip.name
+# # }
+
+# # # Output for Bastion Host
+# # output "bastion_host_name" {
+# #   description = "Name of the Azure Bastion Host"
+# #   value       = azurerm_bastion_host.bastion-host.name
+# # }
+
+# # # Output for Linux Virtual Machine Scale Set
+# # output "linux_vmss_name" {
+# #   description = "Name of the Linux Virtual Machine Scale Set"
+# #   value       = azurerm_linux_virtual_machine_scale_set.app_gateway_web_vmss.name
+# # }
+
+# # # Output for Network Security Group
+# # output "network_security_group_name" {
+# #   description = "Name of the Network Security Group (NSG)"
+# #   value       = azurerm_network_security_group.ag_subnet_nsg.name
+# # }
+
+# # # Output for NSG Rule Inbound Ports
+# # output "inbound_ports_map" {
+# #   description = "Map of NSG inbound rule ports"
+# #   value       = local.ag_inbound_ports_map
+# # }
+
+# # Output for Resource Group
+# output "resource_group_id" {
+#   description = "ID of the Azure Resource Group"
+#   value       = azurerm_resource_group.rg-group.id
 # }
 
-# Output for Resource Group
-output "resource_group_id" {
-  description = "ID of the Azure Resource Group"
-  value       = azurerm_resource_group.rg-group.id
-}
-
-# Output for Virtual Network
-output "virtual_network_id" {
-  description = "ID of the Azure Virtual Network"
-  value       = azurerm_virtual_network.vnet.id
-}
-
-# Output for Subnets
-output "frontend_subnet_id" {
-  description = "ID of the Frontend Subnet"
-  value       = azurerm_subnet.frontend.id
-}
-
-output "backend_subnet_id" {
-  description = "ID of the Backend Subnet"
-  value       = azurerm_subnet.backend.id
-}
-
-output "workload_subnet_id" {
-  description = "ID of the Workload Subnet"
-  value       = azurerm_subnet.workload.id
-}
-
-output "private_ip_test_subnet_id" {
-  description = "ID of the Private IP Test Subnet"
-  value       = azurerm_subnet.private-ip-test.id
-}
-
-# output "bastion_subnet_id" {
-#   description = "ID of the Bastion Host Subnet"
-#   value       = azurerm_subnet.bastion.id
+# # Output for Virtual Network
+# output "virtual_network_id" {
+#   description = "ID of the Azure Virtual Network"
+#   value       = azurerm_virtual_network.vnet.id
 # }
 
-# Output for Log Analytics Workspace
-output "log_analytics_workspace_id" {
-  description = "ID of the Azure Log Analytics Workspace"
-  value       = azurerm_log_analytics_workspace.log_analytics_workspace.id
-}
-
-# # Output for Windows Virtual Machine (Bastion)
-# output "bastion_virtual_machine_id" {
-#   description = "ID of the Windows Virtual Machine (Bastion Host)"
-#   value       = azurerm_windows_virtual_machine.bastion.id
+# # Output for Subnets
+# output "frontend_subnet_id" {
+#   description = "ID of the Frontend Subnet"
+#   value       = azurerm_subnet.frontend.id
 # }
 
-# # Output for Network Interface (Bastion NIC)
-# output "bastion_nic_id" {
-#   description = "ID of the Network Interface (NIC) for Bastion Host"
-#   value       = azurerm_network_interface.bastion_win_vm_nic.id
+# output "backend_subnet_id" {
+#   description = "ID of the Backend Subnet"
+#   value       = azurerm_subnet.backend.id
 # }
 
-# # Output for Public IP (Bastion Public IP)
-# output "bastion_public_ip_id" {
-#   description = "ID of the Public IP Address for Bastion Host"
-#   value       = azurerm_public_ip.bastion_public_ip.id
+# output "workload_subnet_id" {
+#   description = "ID of the Workload Subnet"
+#   value       = azurerm_subnet.workload.id
 # }
 
-# # Output for Bastion Host
-# output "bastion_host_id" {
-#   description = "ID of the Azure Bastion Host"
-#   value       = azurerm_bastion_host.bastion-host.id
+# output "private_ip_test_subnet_id" {
+#   description = "ID of the Private IP Test Subnet"
+#   value       = azurerm_subnet.private-ip-test.id
 # }
 
-# # Output for Linux Virtual Machine Scale Set
-# output "linux_vmss_id" {
-#   description = "ID of the Linux Virtual Machine Scale Set"
-#   value       = azurerm_linux_virtual_machine_scale_set.app_gateway_web_vmss.id
+# # output "bastion_subnet_id" {
+# #   description = "ID of the Bastion Host Subnet"
+# #   value       = azurerm_subnet.bastion.id
+# # }
+
+# # Output for Log Analytics Workspace
+# output "log_analytics_workspace_id" {
+#   description = "ID of the Azure Log Analytics Workspace"
+#   value       = azurerm_log_analytics_workspace.log_analytics_workspace.id
 # }
 
-# # Output for Network Security Group
-# output "network_security_group_id" {
-#   description = "ID of the Network Security Group (NSG)"
-#   value       = azurerm_network_security_group.ag_subnet_nsg.id
+# # # Output for Windows Virtual Machine (Bastion)
+# # output "bastion_virtual_machine_id" {
+# #   description = "ID of the Windows Virtual Machine (Bastion Host)"
+# #   value       = azurerm_windows_virtual_machine.bastion.id
+# # }
+
+# # # Output for Network Interface (Bastion NIC)
+# # output "bastion_nic_id" {
+# #   description = "ID of the Network Interface (NIC) for Bastion Host"
+# #   value       = azurerm_network_interface.bastion_win_vm_nic.id
+# # }
+
+# # # Output for Public IP (Bastion Public IP)
+# # output "bastion_public_ip_id" {
+# #   description = "ID of the Public IP Address for Bastion Host"
+# #   value       = azurerm_public_ip.bastion_public_ip.id
+# # }
+
+# # # Output for Bastion Host
+# # output "bastion_host_id" {
+# #   description = "ID of the Azure Bastion Host"
+# #   value       = azurerm_bastion_host.bastion-host.id
+# # }
+
+# # # Output for Linux Virtual Machine Scale Set
+# # output "linux_vmss_id" {
+# #   description = "ID of the Linux Virtual Machine Scale Set"
+# #   value       = azurerm_linux_virtual_machine_scale_set.app_gateway_web_vmss.id
+# # }
+
+# # # Output for Network Security Group
+# # output "network_security_group_id" {
+# #   description = "ID of the Network Security Group (NSG)"
+# #   value       = azurerm_network_security_group.ag_subnet_nsg.id
+# # }
+
+
+# # output "user_assigned_identity_id" {
+# #   description = "ID of the User Assigned Identity"
+# #   value       = azurerm_user_assigned_identity.appag_umid.id
+# # }
+
+# output "key_vault_id" {
+#   description = "ID of the Azure Key Vault"
+#   value       = azurerm_key_vault.keyvault.id
 # }
 
 
-# output "user_assigned_identity_id" {
-#   description = "ID of the User Assigned Identity"
-#   value       = azurerm_user_assigned_identity.appag_umid.id
+# output "self_signed_certificate_id" {
+#   description = "ID of the self-signed SSL certificate in Azure Key Vault"
+#   value       = azurerm_key_vault_certificate.ssl_cert_id.id
 # }
 
-output "key_vault_id" {
-  description = "ID of the Azure Key Vault"
-  value       = azurerm_key_vault.keyvault.id
-}
-
-
-output "self_signed_certificate_id" {
-  description = "ID of the self-signed SSL certificate in Azure Key Vault"
-  value       = azurerm_key_vault_certificate.ssl_cert_id.id
-}
-
-output "azurerm_key_vault_certificate_secret_id" {
-  value = azurerm_key_vault_certificate.ssl_cert_id.secret_id
-}
+# output "azurerm_key_vault_certificate_secret_id" {
+#   value = azurerm_key_vault_certificate.ssl_cert_id.secret_id
+# }
