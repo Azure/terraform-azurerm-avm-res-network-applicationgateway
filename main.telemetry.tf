7967cd8c08bdf27ce8b122ac41605760d418faed
Mohamed Faizal <mofaizal@microsoft.com>
2024-08-21 19:22:52 +0800

    conflicts that must be resolved

--- main.telemetry.tf
+++ main.telemetry.tf
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-# resource "random_id" "telemetry" {
-#   count = var.enable_telemetry ? 1 : 0
-
-#   byte_length = 4
-# }
-
-# # This is the module telemetry deployment that is only created if telemetry is enabled.
-# # It is deployed to the resource's resource group.
-# resource "azurerm_resource_group_template_deployment" "telemetry" {
-#   count = var.enable_telemetry ? 1 : 0
-
-#   deployment_mode     = "Incremental"
-#   name                = local.telem_arm_deployment_name
-#   resource_group_name = var.resource_group_name
-#   # location            = var.location
-#   template_content = local.telem_arm_template_content
-# }
-
-=======
->>>>>>> edc4a8a5c63b47006a932f49cb5e7e860ba577b7
 data "azurerm_client_config" "telemetry" {
   count = var.enable_telemetry ? 1 : 0
 }
