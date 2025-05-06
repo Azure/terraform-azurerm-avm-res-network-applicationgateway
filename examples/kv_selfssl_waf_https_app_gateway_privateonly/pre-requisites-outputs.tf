d8d04f056b051f93939212e84ac40781060aa74f
Kamaludeen Mohamed Faizal <35946439+mofaizal@users.noreply.github.com>
2025-05-06 12:03:16 +0800

    Version 0.4.1 (#136) (#137)
    This merge includes the following updates:
    
    Azure Application Gateway deployment configured for Private IP only
    
    Updated request_routing_rules variables for enhanced flexibility
    
    Special thanks to Jason Westfall for stepping in and updating the code to support Private IP-only configurations, and to Giorgio Maggiolo for the valuable feedback on the request_routing_rules variable structure.
    
    Fixes #110 and #134
    
    Co-authored-by: jwestfall-MSFT <jwestfall@microsoft.com>
    Co-authored-by: github-actions[bot] <tfmod442916@users.noreply.github.com>

---
+++ examples/kv_selfssl_waf_https_app_gateway_privateonly/pre-requisites-outputs.tf
@@ -0,0 +1,79 @@
+output "azurerm_key_vault_certificate_secret_id" {
+  value = azurerm_key_vault_certificate.ssl_cert_id.secret_id
+}
+
+output "backend_subnet_id" {
+  description = "ID of the Backend Subnet"
+  value       = azurerm_subnet.backend.id
+}
+
+output "backend_subnet_name" {
+  description = "Name of the Backend Subnet"
+  value       = azurerm_subnet.backend.name
+}
+
+# Output for Subnets
+output "frontend_subnet_id" {
+  description = "ID of the Frontend Subnet"
+  value       = azurerm_subnet.frontend.id
+}
+
+# Output for Subnets
+output "frontend_subnet_name" {
+  description = "Name of the Frontend Subnet"
+  value       = azurerm_subnet.frontend.name
+}
+
+output "key_vault_id" {
+  description = "ID of the Azure Key Vault"
+  value       = azurerm_key_vault.keyvault.id
+}
+
+output "private_ip_test_subnet_id" {
+  description = "ID of the Private IP Test Subnet"
+  value       = azurerm_subnet.private_ip_test.id
+}
+
+output "private_ip_test_subnet_name" {
+  description = "Name of the Private IP Test Subnet"
+  value       = azurerm_subnet.private_ip_test.name
+}
+
+# Output for Resource Group
+output "resource_group_id" {
+  description = "ID of the Azure Resource Group"
+  value       = azurerm_resource_group.rg_group.id
+}
+
+# Output for Resource Group
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg_group.name
+}
+
+output "self_signed_certificate_id" {
+  description = "ID of the self-signed SSL certificate in Azure Key Vault"
+  value       = azurerm_key_vault_certificate.ssl_cert_id.id
+}
+
+# Output for Virtual Network
+output "virtual_network_id" {
+  description = "ID of the Azure Virtual Network"
+  value       = azurerm_virtual_network.vnet.id
+}
+
+# Output for Virtual Network
+output "virtual_network_name" {
+  description = "Name of the Azure Virtual Network"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "workload_subnet_id" {
+  description = "ID of the Workload Subnet"
+  value       = azurerm_subnet.workload.id
+}
+
+output "workload_subnet_name" {
+  description = "Name of the Workload Subnet"
+  value       = azurerm_subnet.workload.name
+}
