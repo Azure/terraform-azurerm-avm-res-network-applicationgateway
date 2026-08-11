9fd125e48d3bd36ffe437875b1dc29866cc82fab
Mathews George <mathewsg6@gmail.com>
2026-08-11 16:24:21 +0100

    changed waf subs id

--- examples/simple_waf_http_app_gateway/main.tf
+++ examples/simple_waf_http_app_gateway/main.tf
@@ -112,7 +112,7 @@ module "application_gateway" {
   # Ensure that you have a WAF policy created before enabling WAF on the Application Gateway
   # The use of an external WAF policy is recommended rather than using the classic WAF via the waf_configuration block.
   firewall_policy = {
-    id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${azurerm_resource_group.rg_group.name}/providers/Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies/${azurerm_web_application_firewall_policy.azure_waf.name}"
+    id = "${azurerm_resource_group.rg_group.id}/providers/Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies/${azurerm_web_application_firewall_policy.azure_waf.name}"
   }
   frontend_ip_configurations = [
     {
